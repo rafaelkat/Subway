c6ae23509865850ad15aec4208a3aa6e4945417b
rafaelkat <97891367+rafaelkat@users.noreply.github.com>
2022-06-30 14:57:53 -0300

    Add files via upload

---
+++ subway.js
@@ -0,0 +1,20 @@
+var pc;
+
+
+// function preload(){
+//  }
+
+function setup(){
+
+    createCanvas(505, 320);
+    pc = createSprite(250,160,10,10);
+}
+
+function draw(){
+
+    background("red");
+
+
+
+    drawSprites();
+}
